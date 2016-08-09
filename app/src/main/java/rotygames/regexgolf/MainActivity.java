e09bc33e6cfae1a8f2013fcbab503c1acc7a4266
Rastamas <r.tamas.szabo@gmail.com>
2016-08-09 19:34:07 +0200

    Initialize the project
    Added empty android studio project

---
+++ app/src/main/java/rotygames/regexgolf/MainActivity.java
@@ -0,0 +1,13 @@
+package rotygames.regexgolf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
