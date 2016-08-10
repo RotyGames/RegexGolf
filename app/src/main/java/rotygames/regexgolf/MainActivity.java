0705ff67007452c15fafae0c929ba194725d96b2
Rastamas <r.tamas.szabo@gmail.com>
2016-08-10 20:03:48 +0200

    Add main menu with mock event listeners
    Added buttons and simple onclick functions that tell you what button you
    pressed. Also added exit button, because I hate apps without exit
    buttons.

--- app/src/main/java/rotygames/regexgolf/MainActivity.java
+++ app/src/main/java/rotygames/regexgolf/MainActivity.java
@@ -2,6 +2,8 @@ package rotygames.regexgolf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickPlay(View view) {
+        Toast.makeText(this, "You clicked PLAY!",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickSettings(View view) {
+        Toast.makeText(this, "You clicked SETTINGS!",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickLeaderboard(View view) {
+        Toast.makeText(this, "You clicked LEADERBOARD!",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickAboutUs(View view) {
+        Toast.makeText(this, "You clicked ABOUT US!",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickHelp(View view) {
+        Toast.makeText(this, "You clicked HELP!",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickExit(View view) {
+        finish();
     }
 }
